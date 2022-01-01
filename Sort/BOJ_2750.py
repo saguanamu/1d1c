e13e41a6de128ac11662bc0073e03a9333e732b9
KyeongNa Park <61577469+pkn0814@users.noreply.github.com>
2022-01-02 00:19:58 +0900

    2750 quick sort

--- Sort/BOJ_2750.py
+++ Sort/BOJ_2750.py
@@ -1,12 +1,36 @@
+def quicksort(list):
+    if len(list) <= 1:
+        return list
+    pivot = list[0]
+    left = []
+    right = []
+    for i in list[1:]:
+        if i < pivot:
+            left.append(i)
+        else:
+            right.append(i)
+            
+    return quicksort(left) + [pivot] + quicksort(right)
+
 N = int(input())
 M = []
 for i in range(N):
     M.append(int(input()))
 
-for i in range(len(M)):
-    for j in range(len(M)):
-        if M[i] < M[j]:
-            M[i], M[j] = M[j], M[i]
+# bubble sort
+#for i in range(len(M)):
+#    for j in range(len(M)):
+#        if M[i] < M[j]:
+#            M[i], M[j] = M[j], M[i]
 
-for n in M:
-    print(n)
+
+# 리스트 sort 이용
+#M2 = M.sort()
+#for N in M:
+#    print(N)
+
+
+n = quicksort(M)
+for i in n:
+    print(i)
+
