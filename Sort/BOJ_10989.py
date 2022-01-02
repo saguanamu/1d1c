36aec8770761e1e7b51278e80ff4bab0347db746
KyeongNa Park <61577469+pkn0814@users.noreply.github.com>
2022-01-02 22:48:44 +0900

    10989 fix memory overflow

--- Sort/BOJ_10989.py
+++ Sort/BOJ_10989.py
@@ -1,21 +1,13 @@
 import sys
+# 메모리 초과 오류
+N = int(sys.stdin.readline())
+M = [0] * 10001
 
-def quicksort(list):
-    if len(list) <= 1:
-        return list
-    pivot = list[0]
-    left = []
-    right = []
-    for i in list[1:]:
-        if i < pivot:
-            left.append(i)
-        else:
-            right.append(i)
-    return quicksort(left) + [pivot] + quicksort(right)
+for i in range(N):
+    num_value = int(sys.stdin.readline())
+    M[num_value] = M[num_value] + 1
 
-N = int(input())
-M = [int(sys.stdin.readline()) for i in range(N)]
-
-M2 = quicksort(M)
-for i in M2:
-    print(i)
+for i in range(10001):
+    if M[i] != 0:
+        for j in range(M[i]):
+            print(i)
