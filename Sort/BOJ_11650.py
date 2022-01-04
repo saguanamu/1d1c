4376343cd2490a2dd167638287ae722423312dfe
KyeongNa Park <61577469+pkn0814@users.noreply.github.com>
2022-01-04 21:03:33 +0900

    11650 sorted

---
+++ Sort/BOJ_11650.py
@@ -0,0 +1,13 @@
+import sys
+
+N = int(sys.stdin.readline())
+arr = []
+
+for i in range(N):
+    [x, y] = map(int, sys.stdin.readline().split())
+    arr.append([x, y])
+
+result = sorted(arr)
+
+for i in range(N):
+    print(result[i][0], result[i][1])
