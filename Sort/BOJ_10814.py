6c01488cd74c70b3389353129c8276cab2840038
KyeongNa Park <61577469+pkn0814@users.noreply.github.com>
2022-01-06 23:39:42 +0900

    10814 sort

---
+++ Sort/BOJ_10814.py
@@ -0,0 +1,18 @@
+import sys
+
+N = int(sys.stdin.readline())
+member_list = []
+
+for i in range(N):
+    age, name = map(str, input().split())
+    age = int(age)
+    member_list.append((age, name))
+
+member_list.sort(key = lambda x: x[0])
+
+for i in member_list:
+    print(i[0], i[1])
+
+    
+
+
