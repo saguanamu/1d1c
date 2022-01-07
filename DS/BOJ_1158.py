386263374ab3b187aec8120b96cd17a010096c99
KyeongNa Park <61577469+pkn0814@users.noreply.github.com>
2022-01-08 00:22:30 +0900

    1158 fix

--- DS/BOJ_1158.py
+++ DS/BOJ_1158.py
@@ -1,11 +1,12 @@
 N, K = map(int, input().split())
 arr = [i for i in range(1, N+1)]
+
 human = []
 cur = 0
 
 for j in range(N):
     cur += K-1
-    if cur > len(arr):
+    if cur >= len(arr):
         cur = cur % len(arr)
     human.append(str(arr.pop(cur)))
 print("<", ", ".join(human)[:], ">", sep='')
