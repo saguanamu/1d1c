d74d71588868df2247fc49f5a0ae8764ff7e433e
KyeongNa Park <61577469+pkn0814@users.noreply.github.com>
2022-01-23 22:54:43 +0900

    9663 err

--- Set2/BOJ_9663.py
+++ Set2/BOJ_9663.py
@@ -1,9 +1,26 @@
 import sys
 
+def adjacent(x):
+    for i in range(x):
+        # 같은 열 or 대각선 위치
+        if row[x] == row[i] or abs(row[i] - row[x]) == x-i:
+            return False
+        return True
+
+def dfs(x):
+    global result
+
+    if x == N:
+        result += 1
+        return
+    else:
+        for i in range(N):
+            row[x] = i
+            if adjacent(x):
+                dfs(x+1)
+
 N = int(sys.stdin.readline())
-
-def dfs():
-
-
-while True:
-    
+result = 0
+row = [0] * N
+dfs(0)
+print(result)
